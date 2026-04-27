3837a7250602767d2f17a88cc18debc06cbc2273
Sean Boa <seanboa@gmail.com>
2026-04-27 20:34:42 +0100

    Update require_once paths in login.php

--- www/resources/handlers/login.php
+++ www/resources/handlers/login.php
@@ -5,8 +5,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-require_once __DIR__ . '/resources/classes/DatabaseHandler.php';
-require_once __DIR__ . '/resources/classes/LoginController.php';
+require_once __DIR__ . '/../../resources/classes/DatabaseHandler.php';
+require_once __DIR__ . '/../../resources/classes/LoginController.php';
+
 
 $studentNumber = '';
 $password = '';
