af58ac551f62523c109ed55717e22878e4b5dd06
Sean Boa <seanboa@gmail.com>
2026-04-28 10:28:10 +0100

    Fix require paths in login.php
    Updated require paths for DatabaseHandler and LoginController.

--- www/resources/handlers/login.php
+++ www/resources/handlers/login.php
@@ -5,8 +5,8 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-require_once __DIR__ . '/../resources/classes/DatabaseHandler.php';
-require_once __DIR__ . '/../resources/classes/LoginController.php';
+require_once __DIR__ . '/../classes/DatabaseHandler.php';
+require_once __DIR__ . '/../classes/LoginController.php';
 
 
 $studentNumber = '';
