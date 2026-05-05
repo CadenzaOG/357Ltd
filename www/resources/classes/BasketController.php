d7ccd15dc28948141524ecc284f7829b2b9f5f35
Sean Boa <seanboa@gmail.com>
2026-05-05 12:23:39 +0100

    Update price retrieval in updateTotal method
    Refactor updateTotal method to use getProductById for price retrieval.

--- www/resources/classes/BasketController.php
+++ www/resources/classes/BasketController.php
@@ -31,10 +31,10 @@ class BasketController
         $this->updateTotal();
     }
 
-    private function updateTotal() {
+   private function updateTotal() {
         $total=0;
         foreach ($this->basket as $id => $item) {
-            $price = $this->productController->getProductPrice($id);
+            $price = $this->productController->getProductById($id)->price;
             $productTotal = $price * $item['quantity'];
             $this->basket[$id]['total'] = $productTotal;
             $total += $productTotal;
